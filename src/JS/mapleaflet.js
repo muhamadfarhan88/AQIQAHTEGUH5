82a899e539856fa25e3cc5c05552a7fde7045215
MuhamadFarhan4455 <muhamadfarhan4455@gmail.com>
2024-11-14 22:46:01 +0700

    initial commit

---
+++ src/JS/mapleaflet.js
@@ -0,0 +1,37 @@
+// src/JS/mapleaflet.js
+
+$(document).ready(function() {
+    // Koordinat untuk lokasi Teguh Aqiqah
+    var mapCenter = [-6.276247154826614, 106.87525804336683]; // Koordinat Teguh Aqiqah
+
+    // Inisialisasi Peta
+    var map = L.map('map', {
+        center: mapCenter,
+        zoom: 16,
+        maxZoom: 20
+    });
+
+    // Layer Peta
+    var osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 20,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    var osmHotLayer = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        maxZoom: 20,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+
+    // Kontrol Layer
+    var baseMaps = {
+        "OpenStreetMap": osmLayer,
+        "OpenStreetMap HOT": osmHotLayer
+    };
+
+    L.control.layers(baseMaps).addTo(map);
+    osmLayer.addTo(map); // Set layer default
+
+    // Marker untuk lokasi Teguh Aqiqah
+    var marker = L.marker(mapCenter).addTo(map);
+    marker.bindPopup("<b>Teguh Aqiqah</b>").openPopup();
+});
