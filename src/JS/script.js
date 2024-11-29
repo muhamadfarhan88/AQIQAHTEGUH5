ca7f0e25d47a023d0021c649befce52a47c2d376
MuhamadFarhan4455 <muhamadfarhan4455@gmail.com>
2024-11-29 20:27:17 +0700

    initial commit

--- src/JS/script.js
+++ src/JS/script.js
@@ -14,23 +14,32 @@ $(document).ready(function(){
         adaptiveHeight: true
     });
 });
-// Modal
-document.getElementById('getStarted').addEventListener('click', function() {
-    fetch('modal-content.html')
-        .then(response => response.text())
-        .then(data => {
-            // Tambahkan modal ke body
-            document.body.insertAdjacentHTML('beforeend', data);
-            
-            // Tampilkan modal
-            const modal = document.getElementById('crud-modal-paket-hemat-laki-i');
-            modal.classList.remove('hidden');
 
-            // Tambahkan event listener untuk menutup modal
-            modal.querySelector('[data-modal-toggle="crud-modal-paket-hemat-laki-i"]').addEventListener('click', function() {
-                modal.classList.add('hidden');
-                modal.remove();
-            });
-        })
-        .catch(error => console.error('Error loading modal:', error));
+//modal
+// Ambil semua elemen dengan kelas 'send-order'
+const sendOrderLinks = document.querySelectorAll('.send-order');
+
+// Tambahkan event listener untuk setiap link
+sendOrderLinks.forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault(); // Mencegah perilaku default link
+
+        fetch('modal-content.html')
+            .then(response => response.text())
+            .then(data => {
+                // Tambahkan modal ke body
+                document.body.insertAdjacentHTML('beforeend', data);
+                
+                // Tampilkan modal
+                const modal = document.getElementById('crud-modal-package');
+                modal.classList.remove('hidden');
+
+                // Tambahkan event listener untuk menutup modal
+                modal.querySelector('[data-modal-toggle="crud-modal-package"]').addEventListener('click', function() {
+                    modal.classList.add('hidden');
+                    modal.remove();
+                });
+            })
+            .catch(error => console.error('Error loading modal:', error));
+    });
 });
